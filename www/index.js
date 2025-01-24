062e74cd9dca9bb591e26a68c538b352069eeebd
mayuzee <emilhansbert@gmail.com>
2025-01-24 17:50:01 +0100

    added online/offline detection

--- www/index.js
+++ www/index.js
@@ -317,3 +317,27 @@ function showInfo(message, showTime) {
     if (showTime == null) return;
     setTimeout(removeInfo, showTime);
 }
+
+// online/offline detection
+const network_status_element = document.getElementById('network-status');
+let networkStatus;
+detectNetworkStatus();
+
+window.addEventListener('offline', (event) => {
+    detectNetworkStatus();
+});
+
+window.addEventListener('online', (event) => {
+    detectNetworkStatus();
+});
+
+function detectNetworkStatus() {
+    networkStatus = navigator.onLine;
+    if (networkStatus) {
+        network_status_element.innerText = 'online';
+        network_status_element.style.backgroundColor = '#70dd85';
+    } else {
+        network_status_element.innerText = 'offline';
+        network_status_element.style.backgroundColor = '#d9971c';
+    }
+}
